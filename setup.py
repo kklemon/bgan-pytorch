de87fb3e5cb8e2a9d10e0671946054f90be5fd30
Kristian Klemon <klemon@unternehmertum.de>
2020-07-29 18:11:46 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='bgan-pytorch',
+    version='0.1',
+    description='PyTorch implementation of Boundary Seeking GAN for discrete data generation.',
+    author='Kristian Klemon',
+    author_email='kristian.klemon@gmail.com',
+    packages=['bgan']
+)
